9bcaeec9c376e2ca0a2a1884d88a01e9fe1134f6
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-06-24 16:57:21 -0400

    finishing details

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -38,6 +38,18 @@ namespace RestaurantRater.Controllers
 
             return View(restaurant);
         }
+        // GET: Details {id}
+        public ActionResult Details (int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Restaurant restaurant = _db.Restaurants.Find(id);
+
+            if (restaurant == null)
+                return HttpNotFound();
+
+            return View(restaurant);
+        }
         // GET: Restaurant Edit {id}
         public ActionResult Edit (int? id)
         {
