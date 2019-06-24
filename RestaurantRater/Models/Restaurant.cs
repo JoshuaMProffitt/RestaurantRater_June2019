b4523b5274d512acadcfe7722e672b5c4456ee2f
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-06-24 12:04:50 -0400

    added restaurant index view/action

--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace RestaurantRater.Models
+namespace RestaurantRaterMVC.Models
 {
     public class Restaurant
     {
         public int RestaurantID { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "Food Type")]
         public string FoodType { get; set; }
         public decimal Rating { get; set; }
     }
